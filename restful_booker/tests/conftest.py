247b6f1670174fe27d7a8aee407e315578a3b01a
Arseny Slovtsov <aslovtsov@iikosoftware.com>
2025-10-22 18:45:47 +0300

    Оптимизация импортов

--- restful_booker/tests/conftest.py
+++ restful_booker/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 from faker import Faker
-from constants import HEADERS, BASE_URL
+from restful_booker.tests.constants import HEADERS, BASE_URL
 
 faker = Faker('ru_RU')
 
@@ -14,7 +14,10 @@ def auth_session():
     response = requests.post(
         f"{BASE_URL}/auth",
         headers=HEADERS,
-        json={"username": "admin", "password": "password123"}
+        json={
+            "username": "admin",
+            "password": "password123"
+        }
     )
     assert response.status_code == 200, "Ошибка авторизации"
     token = response.json().get("token")
@@ -23,11 +26,13 @@ def auth_session():
     session.headers.update({"Cookie": f"token={token}"})
     return session
 
+
 @pytest.fixture(scope="session")
 def no_auth_session():
     session = requests.Session()
     session.headers.update(HEADERS)
     return session
+
 
 @pytest.fixture
 def booking_data():
