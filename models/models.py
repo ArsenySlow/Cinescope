92701ffc161e9b229df328658a87502eb5f30020
Arseny Slovtsov <aslovtsov@iikosoftware.com>
2025-11-25 15:05:46 +0300

    RegisterUserResponse verified and banned (was required, now Optional)

--- models/models.py
+++ models/models.py
@@ -47,8 +47,8 @@ class RegisterUserResponse(BaseModel):
     id: str
     email: str = Field(pattern=r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", description="Email пользователя")
     fullName: str = Field(min_length=1, max_length=100, description="Полное имя пользователя")
-    verified: bool
-    banned: bool
+    banned: Optional[bool] = None
+    verified: Optional[bool] = None
     roles: list[Roles]
     createdAt: str = Field(description="Дата и время создания пользователя в формате ISO 8601")
 
